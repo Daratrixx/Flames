768f41a124a25510f485ff35b80ac6baa3c2377b
Daratrixx <antoine.bernelin@studio-otsu.fr>
2018-11-03 11:53:00 +0100

    Layout detection (System language) for player control auto-remapping

--- UnityProject/Assets/GameManager.cs
+++ UnityProject/Assets/GameManager.cs
@@ -1,34 +1,59 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Threading;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour {
 
     public static GameManager singleton;
+    public static PlayerInput AZERTY = null;
+    public static PlayerInput QWERTY = null;
 
     public PlayerController[] characters = new PlayerController[0];
     private int currentCharacterIndex = 0;
 
     // Use this for initialization
     void Start () {
+        InitLayout();
         singleton = this;
-        for(int i = 1; i < characters.Length; ++i) {
+        for (int i = 1; i < characters.Length; ++i) {
             characters[i].enabled = false;
+        }
+        RemapControls();
+    }
+    void InitLayout() {
+        if (AZERTY == null) {
+            AZERTY = Resources.Load<PlayerInput>("ZQSD");
+        }
+        if (QWERTY == null) {
+            QWERTY = Resources.Load<PlayerInput>("WASD");
         }
     }
 
     private void SwapCharacter() {
         if (characters.Length > 0) {
-            Debug.Log("" + currentCharacterIndex + "/" + characters.Length);
             characters[currentCharacterIndex].enabled = false;
             currentCharacterIndex = (currentCharacterIndex + 1) % characters.Length;
-            Debug.Log("" + currentCharacterIndex + "/" + characters.Length);
             characters[currentCharacterIndex].enabled = true;
+            RemapControls();
+        }
+    }
+
+    private void RemapControls() {
+        Debug.Log(Application.systemLanguage);
+        if (isAzerty) {
+            characters[currentCharacterIndex].inputs = AZERTY;
+        } else {
+            characters[currentCharacterIndex].inputs = QWERTY;
         }
     }
 
     public static void SwapCharacters() {
         singleton.SwapCharacter();
+    }
+
+    public static bool isAzerty {
+        get { return Application.systemLanguage == SystemLanguage.French; }
     }
 
 }
